df93bf8a135907435e296e316898bdf5ac59b7fc
HodoHodo <57591576+hodokim@users.noreply.github.com>
2021-05-24 23:50:33 +0900

    InfLearn_Spring Day Five

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,26 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
 
         return new MemberServiceImpl(memberRespository());
     }
-
-    private MemberRepository memberRespository() {
+    @Bean
+    public MemberRepository memberRespository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRespository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
